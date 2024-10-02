c5bfe20f4287cf86671ee2647a0c9e389b9159bd
Pedro Giampietro <pedrogiampietro@hotmail.com>
2024-10-02 17:34:43 -0300

    ajusts redis

--- src/middlewares/cache-redis.ts
+++ src/middlewares/cache-redis.ts
@@ -1,12 +1,32 @@
 import { Request, Response, NextFunction } from "express";
 import { createClient } from "redis";
+import dotenv from "dotenv";
 
-export const redisClient = createClient();
+// Carregar variáveis de ambiente do .env
+dotenv.config();
+
+// Configurar a URL de conexão do Redis
+const redisUrl = `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
+const redisOptions: any = {
+  url: redisUrl,
+};
+
+// Se você estiver usando uma senha, adicione-a às opções
+if (process.env.REDIS_PASSWORD) {
+  redisOptions.password = process.env.REDIS_PASSWORD;
+}
+
+export const redisClient = createClient(redisOptions);
 
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
 
 (async () => {
-  await redisClient.connect();
+  try {
+    await redisClient.connect();
+    console.log("Conectado ao Redis com sucesso!");
+  } catch (err) {
+    console.error("Falha ao conectar ao Redis:", err);
+  }
 })();
 
 const cacheMiddleware = async (
@@ -25,7 +45,6 @@ const cacheMiddleware = async (
     }
   } catch (err) {
     console.error("Erro ao acessar o Redis:", err);
-
     next();
   }
 };
